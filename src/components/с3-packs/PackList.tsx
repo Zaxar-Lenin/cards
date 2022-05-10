dfe2d4901c5f5d013dc3c1b3a0dc696a2e58e50d
Anton689 <zizugum@gmail.com>
2022-05-10 19:43:35 +0300

    final packlist layout

--- src/components/с3-packs/PackList.tsx
+++ src/components/с3-packs/PackList.tsx
@@ -7,26 +7,23 @@ export const PackList = () => {
            <div className={s.packsOptions}>
                <div className={s.optionsButton}>
                    <span>Show packs cards</span>
-                   <div>
+                   <div className={s.buttonsMyAll}>
                        <button>My</button>
                        <button>All</button>
                    </div>
                </div>
                <div className={s.optionsRange}>
                    <span>Number of cards</span>
-                   <div>RANGERANGERANGE</div>
+                   <div>RANGERANGERARANG</div>
                </div>
            </div>
-
-
-
            <div className={s.packList}>
                <h3>Pack list</h3>
                <div className={s.packlistSearch}><input type="text"/>
                    <span><button>Add new pack</button></span>
                </div>
-               <div className={s.packlistTable}>sdfsdfsdfsdf</div>
-               <div className={s.packlistPagination}>asdasd</div>
+               <div className={s.packlistTable}>Здесь таблица</div>
+               <div className={s.packlistPagination}>здесь пагинация</div>
            </div>
        </div>
     );
