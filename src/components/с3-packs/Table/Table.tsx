9d35f73e12261dd576b262d37fca03eaaa1073f1
kirillsheiko <kirillsheiko@gmail.com>
2022-05-11 16:49:42 +0300

    Add buttons in table, add function for correct date, change names in table.

--- src/components/с3-packs/Table/Table.tsx
+++ src/components/с3-packs/Table/Table.tsx
@@ -7,22 +7,28 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import {useAppSelector} from "../../../Hooks/hooks";
+import {ButtonsForPacks} from "../p1-ButtonsForPacks/ButtonsForPacks";
 
 
 
 export const CustomTable = () => {
 
     const cardPacks = useAppSelector(state => state.packList.cardPacks)
+
+    const CorrectData = (data:string): string => {
+        return data.slice(0,10).split('-').reverse().join('.');
+    }
+
     return (
         <TableContainer component={Paper}>
             <Table sx={{ minWidth: 650 }} aria-label="simple table">
                 <TableHead>
                     <TableRow>
-                        <TableCell onClick = {()=>{alert("Hi")}}>Dessert (100g serving)</TableCell>
-                        <TableCell align="right">*</TableCell>
-                        <TableCell align="right">+</TableCell>
-                        <TableCell align="right">=</TableCell>
-                        <TableCell align="right">-</TableCell>
+                        <TableCell onClick = {()=>{alert("Hi")}}>Name</TableCell>
+                        <TableCell align="right">Cards</TableCell>
+                        <TableCell align="right">Last Update</TableCell>
+                        <TableCell align="right">Created by</TableCell>
+                        <TableCell align="left">Actions</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
@@ -35,8 +41,9 @@ export const CustomTable = () => {
                                 {row.name}
                             </TableCell>
                             <TableCell align="right">{row.cardsCount}</TableCell>
-                            <TableCell align="right">{row.updated}</TableCell>
+                            <TableCell align="right">{CorrectData(row.updated)}</TableCell>
                             <TableCell align="right">{row.user_name}</TableCell>
+                            <TableCell align="right">{<ButtonsForPacks/>}</TableCell>
                         </TableRow>
                     ))}
                 </TableBody>
--- src/components/с3-packs/p1-ButtonsForPacks/ButtonsForPacks.tsx
+++ src/components/с3-packs/p1-ButtonsForPacks/ButtonsForPacks.tsx
@@ -3,8 +3,22 @@ import ButtonsStyles from './ButtonsForPacks.module.css'
 
 export const ButtonsForPacks = () => {
     return <div className={ButtonsStyles.mainButtonsStyle}>
-        <Button variant="outlined">Delete</Button>
-        <Button variant="outlined">Edit</Button>
-        <Button variant="outlined">Learn</Button>
+        <Button variant="outlined"
+                size="small"
+        >
+            Delete
+        </Button>
+        <Button
+            variant="outlined"
+            size="small"
+        >
+            Edit
+        </Button>
+        <Button
+            variant="outlined"
+            size="small"
+        >
+            Learn
+        </Button>
     </div>
 }
