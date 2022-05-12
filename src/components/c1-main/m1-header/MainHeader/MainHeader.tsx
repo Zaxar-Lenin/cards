d0a4fdcc4195ce28ab6245c0d11328779f85af55
Anton689 <zizugum@gmail.com>
2022-05-12 03:07:19 +0300

    added header

--- src/components/c1-main/m1-header/MainHeader/MainHeader.tsx
+++ src/components/c1-main/m1-header/MainHeader/MainHeader.tsx
@@ -1,21 +1,51 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import s from './MainHeader.module.css'
+import {useLocation, useNavigate} from 'react-router-dom';
 
 export const MainHeader = () => {
 
-    const [style, setStyle] = useState('')
+    const navigate = useNavigate()
+    const location = useLocation()
 
-    const activeStyleHandler = ()=> {
-        setStyle('active')
+    const [style, setStyle] = useState({
+        packlist: false,
+        profile: true
+    })
+
+    useEffect(() => {
+        if (location.pathname === '/packlist') {
+            setStyle({
+                packlist: true,
+                profile: false,
+            })
+        }
+    }, [])
+
+    const onPacksListHandler = () => {
+        setStyle({
+            packlist: true,
+            profile: false,
+        })
+        navigate('/packlist')
+    }
+
+    const onProfileHandler = () => {
+        setStyle({
+            packlist: false,
+            profile: true,
+        })
+        navigate('/profile')
     }
 
     return (
         <div className={s.headerStyle}>
             <div className={s.buttons}>
-            <button className={style.length > 0 ? `${s}.active` : ''} onClick={activeStyleHandler}>Pack list</button>
-            <button className={s.profileButton}>Profile</button>
+                <button onClick={onPacksListHandler} className={style.packlist ? s.active : s.packlistButton}>Pack
+                    list
+                </button>
+                <button onClick={onProfileHandler} className={style.profile ? s.active : s.profileButton}>Profile
+                </button>
             </div>
         </div>
     );
 };
-
